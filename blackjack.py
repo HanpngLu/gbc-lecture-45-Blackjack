f9d078f9a2965eeb0e296a2c3ca640e864af18d3
Hanping Lu <hanping.lu@aa.com>
2020-04-10 14:19:22 -0500

    pull and push test

--- blackjack.py
+++ blackjack.py
@@ -122,7 +122,7 @@ def shuffle():
     random.shuffle(deck)
 
 def tim():
-    print("this says hi and try more changes")
+    print("this says hi and deleted changes")
 
 mainWindow = tkinter.Tk()
 
